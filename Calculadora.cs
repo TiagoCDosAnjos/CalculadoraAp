14a954599b57120036104b89c8dd6e4e8b650cc1
PC <PC@DESKTOP-1U63K83>
2024-03-26 21:38:08 -0300

    Add project files.

---
+++ Calculadora.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCalculadora
+{
+    internal class Calculadora
+    {
+        public int Numero1 { private get; set; }
+        public int Numero2 { private get; set; }
+
+        public double Soma(double numero1, double numero2)
+        {
+            return numero1 + numero2;
+        }
+
+         public double Subtrair(double numero1, double numero2)
+        {
+            return numero1 - numero2;
+        }
+
+         public double Dividir(double numero1, double numero2)
+        {
+            return numero1 / numero2;
+        }
+
+         public double Multiplicar(double numero1, double numero2)
+        {
+            return numero1 * numero2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using ProjetoCalculadora;
+
+string operacao, continuar;
+double resultado = 0;
+Console.WriteLine();
+Calculadora calc = new Calculadora();
+do
+{
+    try
+    {
+        Console.Clear();
+        Console.WriteLine("Deseja fazer Qual operação? \n \n" +
+
+            "Digite [1] para soma \n" +
+            "Digite [2] para subtrair \n" +
+            "Digite [3] para multiplicar \n" +
+            "Digite [4] para Dividir \n");
+
+        operacao = Console.ReadLine();
+
+        Console.Write("Digite o primeiro numero: ");
+
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo numero: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        switch (operacao)
+        {
+            case "1":
+                resultado = calc.Soma(num1, num2);
+                break;
+            case "2":
+                resultado = calc.Subtrair(num1, num2);
+                break;
+            case "3":
+                resultado = calc.Multiplicar(num1, num2);
+                break;
+            case "4":
+                do
+                {
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("O número digitado não pode ser 0 !");
+                        Console.Write("digite outro número: ");
+                        num2 = double.Parse(Console.ReadLine());
+                    }
+                } while (num2 == 0);
+                resultado = calc.Dividir(num1, num2);
+                break;
+            default:
+                // código
+                break;
+        }
+        //if (operacao == '1')
+        //    resultado = calc.Soma(num1, num2);
+        //else if (operacao == '2')
+        //    resultado = calc.Subtrair(num1, num2);
+        //else if (operacao == '3')
+        //    resultado = calc.Multiplicar(num1, num2);
+        //else if (operacao == '4')
+        //    resultado = calc.Dividir(num1, num2);
+        //else
+        //    resultado = 0;
+
+        Console.WriteLine("O resultado da operação é: " + resultado);
+        Console.WriteLine();
+    }
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("Não é possivel dividir por 0! ");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Digite apenas números válidos!");
+    }
+    Console.Write("Deseja fazer outra operação digite 's' para Sim e 'n' para Não: ");
+    continuar = Console.ReadLine();
+
+} while (continuar == "s" || continuar == "S");
+
+
+
+
+
